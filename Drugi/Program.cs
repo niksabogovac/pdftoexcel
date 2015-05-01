2a47a312cee16b041a8458659bef982fd3163509
Niksa Bogovac <niksa@pbssolutions.net>
2015-05-01 22:07:18 +0200

    init commit

---
+++ Drugi/Program.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelLibrary;
+
+using QiHe.CodeLib;
+using ExcelLibrary.SpreadSheet;
+
+using System.Text.RegularExpressions;
+
+namespace Drugi
+{
+    class Program
+    {
+        #region Variable definitions
+        public static string path = "";
+        public static string outPath = "";
+
+        public static Workbook book;
+        public static Worksheet sheet;
+        public static Worksheet outputsheet;
+        public static Workbook outputBook;
+
+        public static Regex regContCode = new Regex(@"[0-9]+");
+        public static Regex regDate = new Regex(@"[0-9]+\.[0-9]+\.-[0-9]+\.[0-9]+\.");
+        public static Regex regDateExtended = new Regex(@"[0-9]+-[0-9]+-[0-9]+\/[0-9]+\.[0-9]+\.-[0-9]+\.[0-9]+\.");
+        public static Regex regYear = new Regex(@"20[0-9]{2}");
+
+        public static string Page = "";
+        public static string Account = "";
+        public static string Name = "";
+        public static string ContainerCode = "";
+        public static string Year = "";
+        public static string Description = "";
+        public static string Contents = "";
+        public static string Remarks = "";
+        public static string Details = "";
+
+
+
+        public static int curRow;
+        public static int curCol;
+        #endregion
+
+        static void Main(string[] args)
+        {
+            #region Preparing input and output files
+            
+            // Create input and output path
+            path += Application.StartupPath;
+            outPath += Application.StartupPath + @"\output.xls";
+            Console.WriteLine("Unesite ime ulaznog xls fajla: ");
+            string filename = "";
+            filename = Console.ReadLine();
+            path += @"\" + filename + ".xls";
+            
+
+            // Open WorkBook with input path
+            book = new Workbook();
+            try
+            {
+                book = Workbook.Load(path);
+            }
+            catch
+            {
+                Console.WriteLine("Uneti fajl ne moze da se otvori.\nProverite da li ste uneli ispravno ime.\nProverite da li je vec otvoren i ukoliko jeste zatvorite ga i pokusajte ponovo.");
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+
+            // Open first sheet in input xls document 
+            try
+            {
+                sheet = book.Worksheets[0];
+            }
+            catch
+            {
+                Console.WriteLine("Uneti fajl ne moze da se otvori.\nUlazni fajl mora da sadrzi sve u prvom sheetu!");
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+
+
+            // Create default output sheet and workbook
+            outputsheet = new Worksheet("OutPut");
+            outputBook = new Workbook();
+
+            // Set it to beginning of the document
+            curRow = 0;
+            curCol = 0;
+
+            // Write default headers
+            outputsheet.Cells[curRow, curCol++] = new Cell("Page");
+            outputsheet.Cells[curRow, curCol++] = new Cell("Account");
+            outputsheet.Cells[curRow, curCol++] = new Cell("Name");
+            outputsheet.Cells[curRow, curCol++] = new Cell("ContainerCode");
+            outputsheet.Cells[curRow, curCol++] = new Cell("Year");
+            outputsheet.Cells[curRow, curCol++] = new Cell("Description");
+            outputsheet.Cells[curRow, curCol++] = new Cell("Contents");
+            outputsheet.Cells[curRow, curCol++] = new Cell("Details");
+            outputsheet.Cells[curRow++, curCol] = new Cell("Napomena");
+            curCol = 0;
+
+            #endregion
+
+            #region Main loop
+            for (int rowIndex = sheet.Cells.FirstRowIndex; rowIndex <= sheet.Cells.LastRowIndex; rowIndex++)
+            {
+                // Read row by row and check if any of rules are fulfilled
+                Row row = new Row();
+                row = sheet.Cells.GetRow(rowIndex);
+
+
+                string rowStr = "";
+                joinRow(ref rowStr, row);
+
+                if (rowStr.Contains("Page"))
+                {
+                    managePage(rowStr);
+                    continue;
+                }
+
+                /*if (rowStr.Contains("Year"))
+                {
+                    manageYear(ref Year, rowStr);
+                    continue;
+                }*/
+                if (rowStr.Contains("Account:"))
+                {
+                    manageAccAndName(rowStr, rowIndex);
+                    continue;
+                }
+
+                if (rowStr.Contains("Description:"))
+                {
+                    manageDesc(rowStr);
+                    continue;
+                }
+
+
+                if (regContCode.IsMatch(row.GetCell(0).StringValue))
+                {
+                    manageContCode(row);
+                    continue;
+                }
+
+                if (rowStr.Contains("Contents") || rowStr.StartsWith("-"))
+                {
+                    manageCont(rowIndex, rowStr);
+                    continue;
+                }
+
+                if (regDate.IsMatch(rowStr) || regDateExtended.IsMatch(rowStr))
+                {
+                    manageDetails(rowStr, rowIndex);
+                    continue;
+                }
+            }
+            #endregion
+
+            #region Write to output file
+            try
+            {
+                outputBook.Worksheets.Add(outputsheet);
+                outputBook.Save(outPath);
+            }
+            catch
+            {
+                Console.WriteLine("Nije moguce upisati u izlazni fajl.\nOn se kreira u datoteci gde je i ulazni.\nZatvorite ga i pokusajte ponovo.");
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+            Console.WriteLine("Uspesno kreirana izlazna datoteka pod imenom output!");
+            #endregion
+        }
+           
+
+        private static void manageYear(string rowStr)
+        {
+            try
+            {
+                string tmp = "";
+                string[] stringSeparators = new string[] { "Year" };
+                string[] tokens = rowStr.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+                tmp = tokens[1];
+                Year = Regex.Replace(tmp, "-", "");
+            }
+            catch { }
+        }
+
+        private static void managePage(string rowStr)
+        {
+            string tmp = "";
+            string[] stringSeparators = new string[] { "Page" };
+            string[] tokens = rowStr.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+            tmp = tokens[1];
+            Page = Regex.Replace(tmp, "-", "");
+        }
+
+        private static void manageAccAndName(string rowStr, int rowIndex)
+        {
+            string[] stringSeparators;
+            string[] tokens;
+            if (rowStr.Contains("Name:"))
+            {
+                stringSeparators = new string[] { "Name:" };
+                tokens = rowStr.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+                Name = tokens[1];
+                stringSeparators = new string[] { "Account:" };
+                tokens = tokens[0].Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+                Account = tokens[0];
+            }
+            else
+            {
+                stringSeparators = new string[] { "Account:" };
+                tokens = rowStr.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+                Account = tokens[0];
+
+                Row nextRow = sheet.Cells.GetRow(rowIndex + 1);
+                string nextRowStr = "";
+                joinRow(ref nextRowStr, nextRow);
+
+                if (nextRowStr.Contains("Name:"))
+                {
+                    stringSeparators = new string[] { "Name:" };
+                    tokens = rowStr.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+                    Name = tokens[0];
+                }
+            }
+
+            Description = "";
+            Contents = "";
+            Details = "";
+            Remarks = "";
+
+        }
+
+        private static void manageContCode(Row row)
+        {
+            ContainerCode = row.GetCell(0).StringValue;
+
+            // Does it contain Year?
+            for (int i = row.FirstColIndex; i <= row.LastColIndex; i++)
+            {
+                if (regYear.IsMatch(row.GetCell(i).StringValue))
+                {
+                    Year = row.GetCell(i).StringValue;
+                }
+            }
+        }
+
+        private static void manageDesc(string rowStr)
+        {
+
+            string tmp = "";
+            string[] stringSeparators = new string[] { "Description:" };
+            string[] tokens = rowStr.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+            tmp = tokens[0];
+            Description = Regex.Replace(tmp, "-", "");
+
+        }
+
+        private static void manageCont(int rowIndex, string rowStr)
+        {
+            if (rowStr.StartsWith("-"))
+            {
+                Contents = rowStr.Substring(1);
+                Row nextRow = sheet.Cells.GetRow(rowIndex + 1);
+                string nextRowStr = "";
+                joinRow(ref nextRowStr, nextRow);
+                if (nextRowStr.StartsWith("-"))
+                {
+                    printToSheet();
+                }
+            }
+            else
+            {
+
+                string tmp = "";
+                string[] stringSeparators = new string[] { "Contents:" };
+                string[] tokens = rowStr.Split(stringSeparators,StringSplitOptions.RemoveEmptyEntries);
+                tmp = tokens[0];
+                Contents = Regex.Replace(tmp, "-", "");
+
+                Row nextRow = sheet.Cells.GetRow(rowIndex + 1);
+                string nextRowStr = "";
+                joinRow(ref nextRowStr, nextRow);
+                if (nextRowStr.StartsWith("-"))
+                {
+                    printToSheet();
+                }
+            }
+
+        }
+
+        private static void manageDetails(string rowStr, int rowIndex)
+        {
+            Details = rowStr;
+            Row nextRow = sheet.Cells.GetRow(rowIndex + 1);
+            string nextRowStr = "";
+            joinRow(ref nextRowStr, nextRow);
+
+            if (!(nextRowStr.Contains("Account") || nextRowStr.Contains("Name:") || nextRowStr.Contains("Description")
+                || regContCode.IsMatch(nextRowStr)
+                || nextRowStr.Contains("Contents")
+                || regDate.IsMatch(nextRowStr) || regDateExtended.IsMatch(nextRowStr) || nextRowStr.StartsWith("-") || nextRowStr.Contains("Page") || nextRowStr.Contains("Destroyed")))
+            {
+                Remarks = nextRowStr;
+
+                printToSheet();
+                
+
+                Remarks = "";
+                Details = "";
+            }
+            else
+            {
+                printToSheet();
+                
+                Details = "";
+            }
+
+
+        }
+
+        private static void joinRow(ref string rowStr, Row row)
+        {
+            for (int i = row.FirstColIndex; i <= row.LastColIndex; i++)
+            {
+                rowStr += row.GetCell(i).StringValue;
+            }
+            rowStr = Regex.Replace(rowStr, @"\s+", "");
+        }
+
+        private static void printToSheet()
+        {
+            outputsheet.Cells[curRow, curCol++] = new Cell(Page);
+            outputsheet.Cells[curRow, curCol++] = new Cell(Account);
+            outputsheet.Cells[curRow, curCol++] = new Cell(Name);
+            outputsheet.Cells[curRow, curCol++] = new Cell(ContainerCode);
+            outputsheet.Cells[curRow, curCol++] = new Cell(Year);
+            outputsheet.Cells[curRow, curCol++] = new Cell(Description);
+            outputsheet.Cells[curRow, curCol++] = new Cell(Contents);
+            outputsheet.Cells[curRow, curCol++] = new Cell(Details);
+            outputsheet.Cells[curRow++, curCol++] = new Cell(Remarks);
+            // Return to first item in row
+            curCol = 0;
+        }
+
+    }
+}
